bc3c6a63b55a8d854cc186fbeaf440f556f24771
Philipp Lypniakov <filipprayden@gmail.com>
2018-08-25 23:18:49 +0300

    using fetch isntead of robots.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends Component {
@@ -14,7 +13,9 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.setState({ robots: robots});
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}));
     }
 
     onSearchChange = (event) => {
