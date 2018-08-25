95ff24c7506eb0a5f8c55eefd9bb51282e22ad01
Philipp Lypniakov <filipprayden@gmail.com>
2018-08-25 22:32:08 +0300

    React Lectures 1-3 & 'Styling of the App'

---
+++ src/robots.js
@@ -0,0 +1,76 @@
+export const robots = [{
+        "id": 1,
+        "name": "Judi Beaston",
+        "email": "jbeaston0@elpais.com"
+    },
+    {
+        "id": 2,
+        "name": "Annette Flather",
+        "email": "aflather1@oaic.gov.au"
+    },
+    {
+        "id": 3,
+        "name": "Trista Gilks",
+        "email": "tgilks2@mlb.com"
+    },
+    {
+        "id": 4,
+        "name": "Natty Mahedy",
+        "email": "nmahedy3@stumbleupon.com"
+    },
+    {
+        "id": 5,
+        "name": "Rickert Leacock",
+        "email": "rleacock4@stumbleupon.com"
+    },
+    {
+        "id": 6,
+        "name": "Alejoa Bunton",
+        "email": "abunton5@seattletimes.com"
+    },
+    {
+        "id": 7,
+        "name": "Ceciley Hoyland",
+        "email": "choyland6@cnn.com"
+    },
+    {
+        "id": 8,
+        "name": "Derward Torre",
+        "email": "dtorre7@smh.com.au"
+    },
+    {
+        "id": 9,
+        "name": "Olag Fogarty",
+        "email": "ofogarty8@seattletimes.com"
+    },
+    {
+        "id": 10,
+        "name": "Kinsley Blondelle",
+        "email": "kblondelle9@sphinn.com"
+    },
+    {
+        "id": 11,
+        "name": "Dawna Rallinshaw",
+        "email": "drallinshawa@ox.ac.uk"
+    },
+    {
+        "id": 12,
+        "name": "Michail Quick",
+        "email": "mquickb@alibaba.com"
+    },
+    {
+        "id": 13,
+        "name": "Hilton Batter",
+        "email": "hbatterc@liveinternet.ru"
+    },
+    {
+        "id": 14,
+        "name": "Franklin Insoll",
+        "email": "finsolld@amazonaws.com"
+    },
+    {
+        "id": 15,
+        "name": "Weider Dawltrey",
+        "email": "wdawltreye@google.ru"
+    }
+]
