2ad773c87cf6b84064a5b48ec9a56bfbc672b9b3
Philipp Lypniakov <filipprayden@gmail.com>
2019-01-11 15:43:54 +0200

    Added new constants, actions & reducer to manage robots array

--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,42 @@
-import CHANGE_SEARCH_FIELD from './constants.js';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
   searchField: ''
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       // return { ...state, ...payload } --> ES2018 way of merging objects
       return Object.assign({}, state, { searchField: action.payload });
 
+    default:
+      return state;
+  }
+};
+
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+};
+
+export const requestRobots = (
+  state = initialStateRobots,
+  { type, payload } = {}
+) => {
+  switch (type) {
+    case REQUEST_ROBOTS_PENDING:
+      return { ...state, ...{ isPending: true } };
+    case REQUEST_ROBOTS_SUCCESS:
+      return { ...state, ...{ robots: payload, isPending: false } };
+    case REQUEST_ROBOTS_FAILED:
+      return { ...state, ...{ error: payload, isPending: false } };
     default:
       return state;
   }
