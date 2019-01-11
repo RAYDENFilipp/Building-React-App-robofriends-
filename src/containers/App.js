99f4b60df53cd2384427ed4ae56aeeb6260f6cca
Philipp Lypniakov <filipprayden@gmail.com>
2019-01-11 13:18:05 +0200

    Connected react App to redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchField: ''
+      // searchField: '' - it comes as props
     };
   }
 
@@ -19,13 +33,14 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ robots: users }));
   }
-
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
+// no need with redux
+  // onSearchChange = event => {
+  //   this.setState({ searchField: event.target.value });
+  // };
 
   render() {
-    const { robots, searchField } = this.state;
+    const { robots, /* searchField */ } = this.state;
+    const { searchField, onSearchChange } = this.props; // searchField comes down as props
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
@@ -34,7 +49,7 @@ class App extends Component {
     ) : (
       <div className="tc fixed w-100">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -44,5 +59,8 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+//higher order function. Connect() returns another fn that accepts App as a parameter
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/reducers.js
+++ src/reducers.js
@@ -2,16 +2,15 @@ import CHANGE_SEARCH_FIELD from './constants.js';
 
 const initialState = {
   searchField: ''
-}
+};
 
 export const searchRobots = (state = initialState, action = {}) => {
   switch (action.type) {
-
     case CHANGE_SEARCH_FIELD:
-    // return { ...state, ...payload } --> ES2018 way of merging objects
-      return Object.assign({}, state, {searchField: action.payload});
+      // return { ...state, ...payload } --> ES2018 way of merging objects
+      return Object.assign({}, state, { searchField: action.payload });
 
     default:
       return state;
   }
-}
+};
