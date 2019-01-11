6b98d6a46185c1b0d71406ef8ab9fa15d6f6d6a1
Philipp Lypniakov <filipprayden@gmail.com>
2019-01-11 17:12:11 +0200

    Added action requestRobots to App.js and made it to be a higher order function

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,45 +6,49 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   };
 };
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      // searchField: '' - it comes as props
-    };
-  }
+  // constructor not needed anymore
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: []
+  //     searchField: ''
+  //   };
+  // }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
   }
-// no need with redux
+  // no need with redux
   // onSearchChange = event => {
   //   this.setState({ searchField: event.target.value });
   // };
 
   render() {
-    const { robots, /* searchField */ } = this.state;
-    const { searchField, onSearchChange } = this.props; // searchField comes down as props
+    // not needed anymore
+    // const { robots, searchField } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props; // robots and searchField come down as props
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return !robots.length ? (
+    return isPending ? (
       <h1>Loading</h1>
     ) : (
       <div className="tc fixed w-100">
